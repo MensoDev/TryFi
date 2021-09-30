d8d637d3ad274ec7d0e8c8e5bcc3897301b33ab7
Emerson Trindade <emersont@outlook.com.br>
2021-09-30 18:01:19 -0300

    Implementing a Command and Handler for Plan entity

---
+++ src/TryFi.Hotspot.Application/Commands/RegisterPlanCommand.cs
@@ -0,0 +1,48 @@
+﻿using FluentValidation;
+using TryFi.Kernel.Domain.Messages;
+
+namespace TryFi.Hotspot.Application.Commands
+{
+    public class RegisterPlanCommand : Command
+    {
+        public RegisterPlanCommand(string name, string upload, string download)
+        {
+            Name = name;
+            Upload = upload;
+            Download = download;
+        }
+
+        public string Name { get; private set; }
+        public string Upload { get; private set; }
+        public string Download { get; private set; }
+
+
+        public override bool IsValid()
+        {
+            ValidationResult = new RegisterPlanCommandValidator().Validate(this);
+            return ValidationResult.IsValid;
+        }
+    }
+
+    internal class RegisterPlanCommandValidator : AbstractValidator<RegisterPlanCommand>
+    {
+        public RegisterPlanCommandValidator()
+        {
+            RuleFor(x => x.Name)
+                .NotEmpty()
+                .WithMessage("Name is required");
+
+            RuleFor(x => x.Name)
+                .MaximumLength(3)
+                .WithMessage("The name must be at least three characters");
+
+            RuleFor(x => x.Download)
+                .NotEmpty()
+                .WithMessage("Download rate is required");
+
+            RuleFor(x => x.Upload)
+                .NotEmpty()
+                .WithMessage("Upload rate is required");
+        }
+    }
+}
---
+++ src/TryFi.Hotspot.Application/Handlers/PlanCommandHandlers.cs
@@ -0,0 +1,49 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TryFi.Hotspot.Application.Commands;
+using TryFi.Hotspot.Domain.Entities;
+using TryFi.Hotspot.Domain.Repositories;
+using TryFi.Kernel.Domain.Communication.Mediator;
+using TryFi.Kernel.Domain.Extensions;
+
+namespace TryFi.Hotspot.Application.Handlers
+{
+    public class PlanCommandHandlers :
+        IRequestHandler<RegisterPlanCommand, bool>
+
+    {
+        private readonly IPlanRepository _planRepository;
+        private readonly IMediatorHandler _mediatorHandler;
+
+        public PlanCommandHandlers(IPlanRepository planRepository, IMediatorHandler mediatorHandler)
+        {
+            _planRepository = planRepository;
+            _mediatorHandler = mediatorHandler;
+        }
+
+        public async Task<bool> Handle(RegisterPlanCommand request, CancellationToken cancellationToken)
+        {
+            if (!_mediatorHandler.ValidateCommand(request)) return false;
+
+            //TODO: implement: verification if there is a registered plan
+
+            Plan plan = new Plan(request.Name, request.Upload, request.Download);
+
+            //TODO: Register events on the entity
+
+            await _planRepository.RegisterPlanAsync(plan);
+
+            if (!await _planRepository.UnitOfWork.CommitAsync())
+            {
+                await _mediatorHandler.PublishNotificationAsync("RegisterPlanCommand Handler", "No Registered plan in data context");
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
